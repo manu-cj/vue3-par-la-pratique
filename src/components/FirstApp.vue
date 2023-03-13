584bdc07c821ee8f59b17e026f28d5e3a4be26b0
manucarenjot <carenjotje@gmail.com>
2023-03-13 18:31:31 +0100

    Initial commit

---
+++ src/components/FirstApp.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <p>Date fr :</p> {{dateFr}}<br><br>
+
+    <p>Date  Us:</p> {{dateUs}}<br><br>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FirstApp",
+
+      data() {
+        return {
+          d: new Date(),
+        }
+      },
+  computed: {
+    dateFr() {
+      return this.d.toLocaleTimeString();
+    },
+
+    dateUs() {
+      return this.d.toLocaleTimeString('en-US')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
