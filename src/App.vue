2a7f373a4ede7122a4e262fab43bf9007ff1a81b
manucarenjot <carenjotje@gmail.com>
2023-04-03 18:02:38 +0200

    props

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,31 @@
 <script setup>
 // import FirstApp from "./components/FirstApp.vue";
-import PartTwo from "./components/PartTwo.vue";
+// import PartTwo from "./components/PartTwo.vue";
+import SayHello from "./components/SayHello.vue";
+import BonjourLoc from "./components/BonjourLoc.vue";
+
+
+
+
+
+
+
+
 
 
 </script>
 
-<template>
+<template id="app">
   <header>
-<!--      <FirstApp/>-->
-    <part-two/>
+    <div>
+      <!--      <FirstApp/>-->
+      <!--    <part-two/>-->
+      <SayHello/>
+      <BonjourLoc number="10" ></BonjourLoc>
+      <BonjourLoc number="100" ></BonjourLoc>
+      <BonjourLoc number="1000" ></BonjourLoc>
+    </div>
+
   </header>
 
   <main>
---
+++ src/components/BonjourLoc.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>{{nombre}}</div>
+
+</template>
+
+<script>
+export default {
+  name: "BonjourLoc",
+  data() {
+    return {
+      nbrClics : 0,
+      nombre : Math.floor(Math.random() * this.number +1)
+    }
+  },
+  props : ['number'],
+
+}
+</script>
+
+<style scoped>
+
+</style>
