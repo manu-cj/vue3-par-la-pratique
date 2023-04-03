6fa4cd41b9b9fc77d2937fd00b405314d10da459
manucarenjot <carenjotje@gmail.com>
2023-04-03 22:30:12 +0200

    premier pas avec Axios

--- src/components/UseAxios.vue
+++ src/components/UseAxios.vue
@@ -1,27 +1,34 @@
 <template>
-  <div v-for="user in users" :key="user">
-    <img :src="user.picture.medium" alt="">
-    <p>{{user.name.first}} {{user.name.last}}</p>
-    <p>{{user.email}}</p>
-    <p>{{user.cell}}</p>
-  </div>
+  <table v-for="user in users" :key="user">
+    <tr>
+      <td>
+        <img :src="user.picture.large" alt="">
+      </td>
+      <td>
+        {{ user.name.first }} {{ user.name.last }}
+        <br>
+        {{ user.email }}
+        <br>
+        {{ user.cell }}
+      </td>
+    </tr>
+  </table>
   <br><br>
 </template>
 
 <script>
 import axios from 'axios';
+
 export default {
   name: "UseAxios",
-  data(){
+  data() {
     return {
-      users : [],
+      users: [],
     }
   },
-  methods: {
-
-  },
+  methods: {},
   created() {
-    axios.get('https://randomuser.me/api/?results=10').then( response => {
+    axios.get('https://randomuser.me/api/?results=10').then(response => {
       console.log(response.data.results);
       this.users = response.data.results
     })
