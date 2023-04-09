d4aa34faf2336183a7f718037e3090d6a0964203
manucarenjot <carenjotje@gmail.com>
2023-04-09 16:08:18 +0200

    Utilisation du watcher

--- src/components/ListeEmployer.vue
+++ src/components/ListeEmployer.vue
@@ -1,7 +1,16 @@
 <template>
-  <div v-for="personne in personnes" :key="personne">
-      <slot :personne="personne"></slot>
-  </div>
+<!--  <div v-for="personne in personnes" :key="personne">-->
+<!--      <slot :personne="personne"></slot>-->
+<!--  </div>-->
+<!--  <div>-->
+<!--    <p v-if="compteur < 9">{{texte="Le compteur est de : "}}{{compteur}}</p>-->
+<!--    <p v-else>{{texte="Stop tu va revenir à 0"}}</p>-->
+<!--    <button @click="compteur++">Incrémenter</button>-->
+<!--  </div>-->
+  <input type="text" v-model="msg">
+  <p v-show="affiche"
+  style="color: #00d58a"> Est entrain d'écrire ...
+  </p>
 </template>
 
 <script>
@@ -19,7 +28,30 @@ export default {
           nom: 'Dumanier',
         }
 
-      ]
+      ],
+      compteur : 0,
+      texte : '',
+      msg : '',
+      affiche : false,
+      t : null,
+    }
+  },
+
+  watch : {
+    compteur(valeur) {
+      if (valeur===10) {
+        this.compteur =0;
+      }
+    },
+
+    msg() {
+      if (this.affiche) {
+        clearTimeout(this.t);
+      }
+      this.affiche = true;
+      this.t = setTimeout( ()=> {
+        this.affiche = false
+      }, 3000)
     }
   }
 }
